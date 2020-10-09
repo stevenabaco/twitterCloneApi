5f64f50c7a879e14bc8ecfc55a8c009f37fb9df7
Steven Abaco <stevenabaco@gmail.com>
2020-10-09 15:39:06 -0500

    Move the API Token to .env file and import it

--- api/helpers/twitter.js
+++ api/helpers/twitter.js
@@ -1,18 +1,18 @@
-const axios = require('axios');
-const URL = "https://api.twitter.com/1.1/search/tweets.json"
+const axios = require("axios");
+const URL = "https://api.twitter.com/1.1/search/tweets.json";
 
 class Twitter {
-    get(query, count) {
-        return axios.get(URL, {
-            params: {
-                q: query,
-                count: count,
-            },
-            headers: {
-                Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAAB1kIQEAAAAAsag28gsbEG%2BkKf7HT%2Bzgze0m0fs%3DLOZxOAaL6SObV4q68suzDYtdxLExmyk9ynIuB5HJFpjxVMu0eU",
-            }
-        })
-    }
+	get(query, count) {
+		return axios.get(URL, {
+			params: {
+				q: query,
+				count: count,
+			},
+			headers: {
+				Authorization: `Bearer ${process.env.TWITTER_API_TOKEN}`,
+			},
+		});
+	}
 }
 
-module.exports = Twitter
+module.exports = Twitter;
